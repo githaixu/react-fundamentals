24b4a14c627c060669db5965f157a60258ac763b
Jhin <engkim0826@hotmail.com>
2021-03-25 21:21:33 +0900

    prettiered

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,25 +1,27 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import { italic, red } from 'chalk';
+import {italic, red} from 'chalk'
 import * as React from 'react'
 import '../box-styles.css'
 
 function Box({children, size, style}) {
-  const classList = `box box--${size}`;
+  const classList = `box box--${size}`
   const mergedStyle = {
     fontStyle: 'italic',
     color: 'red',
-    ...style
-  };
+    ...style,
+  }
   return (
-    <div className={classList} style={{...mergedStyle}}>{children}</div>
+    <div className={classList} style={{...mergedStyle}}>
+      {children}
+    </div>
   )
 }
 function App() {
   return (
     <div>
-      <Box size='small' style={{backgroundColor: 'lightblue'}}>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
         small lightblue box
       </Box>
     </div>
