f6cdf2a77ad15e82d83220db6276d02f0ce2d55a
Jhin <engkim0826@hotmail.com>
2021-03-25 21:13:15 +0900

    lesson5 extracredit 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,27 +1,28 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
+import { italic, red } from 'chalk';
 import * as React from 'react'
 import '../box-styles.css'
 
-// 💰 Use the className for the size and style (backgroundColor) for the color
-// 💰 each of the elements should also have the "box" className applied
-
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div className='box box--small' style={{backgroundColor: 'lightblue'}}>small lightblue box</div>
-const mediumBox = <div className='box box--medium' style={{backgroundColor: 'pink'}}>medium pink box</div>
-const largeBox = <div className='box box--large' style={{backgroundColor: 'orange'}}>large orange box</div>
-
+function Box({children, className, style}) {
+  const classList = `box ${className}`;
+  const mergedStyle = {
+    fontStyle: 'italic',
+    color: 'red',
+    ...style
+  };
+  console.log(mergedStyle);
+  return (
+    <div className={classList} style={{...mergedStyle}}>{children}</div>
+  )
+}
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className='box--small' style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
     </div>
   )
 }
